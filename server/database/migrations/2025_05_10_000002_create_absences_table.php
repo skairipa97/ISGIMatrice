3a62ca6a4c04c1896475f0f0950f8bcfa85b81df
iisha123 <iisha.fatihi@gmail.com>
2025-05-14 13:30:34 +0100

    migration ajoutés

---
+++ server/database/migrations/2025_05_10_000002_create_absences_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seance_id')->constrained('seances')->onDelete('cascade');
+            $table->string('stagiaire_matricule');
+            $table->foreign('stagiaire_matricule')->references('matricule')->on('stagiaires')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absences');
+    }
+}; 
